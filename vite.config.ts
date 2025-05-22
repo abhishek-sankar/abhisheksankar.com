34ec1cc315d75ff884c95a90ff0a66eeffa70967
Abhishek Sankar <abhishek.sankar.in@gmail.com>
2025-05-22 16:09:30 -0400

    Indexing changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
+  }
 })
