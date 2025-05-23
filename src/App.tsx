e6125e0c291a51ea7ef5035747a1c39fd2aa8921
Abhishek Sankar <abhishek.sankar.in@gmail.com>
2025-05-22 23:21:37 -0400

    Added volunteering section, need to update urls

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BlogSummary } from './components/BlogSummary'
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ProjectDetail, ProjectList } from './components/ProjectPages';
 import { BlogList, BlogDetail } from './components/BlogPages';
+import { EngagementDetail, EngagementList, PublicEngagements } from './components/PublicEngagements';
 
 function ProfileCardWithRouteControl() {
   const location = useLocation();
@@ -22,11 +23,13 @@ function App() {
         <main className="w-full max-w-3xl px-4 sm:px-8 py-10 flex-1 flex flex-col">
           <ProfileCardWithRouteControl />
           <Routes>
-            <Route path="/" element={<><div className="border-b border-gray-200 dark:border-neutral-800 mb-10"></div><ProfileSummary /><div className="border-b border-gray-200 dark:border-neutral-800 mb-10"></div><BlogSummary /></>} />
+            <Route path="/" element={<><div className="border-b border-gray-200 dark:border-neutral-800 mb-10"></div><ProfileSummary /><div className="border-b border-gray-200 dark:border-neutral-800 mb-10"></div><BlogSummary /><div className="border-b border-gray-200 dark:border-neutral-800 mb-10"></div><PublicEngagements /></>} />
             <Route path="/projects" element={<ProjectList />} />
             <Route path="/projects/:id" element={<ProjectDetail />} />
             <Route path="/blogs" element={<BlogList />} />
             <Route path="/blogs/:id" element={<BlogDetail />} />
+            <Route path="/engagements" element={<EngagementList />} />
+            <Route path="/engagements/:id" element={<EngagementDetail />} />
           </Routes>
         </main>
         <footer className="w-full text-center text-gray-400 text-sm mt-16 border-t border-gray-100 dark:border-neutral-800 pt-6 pb-4">
--- src/components/blogs.ts
+++ src/components/blogs.ts
@@ -1,4 +1,4 @@
-import { Blog_CET_Placements } from "../blogs/Blog_CET_Placements.tsx";
-import { Blog_Anything_API } from "../blogs/Blog_Anything_API.tsx";
+import { Blog_CET_Placements } from "../blogs/Blog_CET_Placements";
+import { Blog_Anything_API } from "../blogs/Blog_Anything_API";
 
 export { Blog_CET_Placements, Blog_Anything_API };
