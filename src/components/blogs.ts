d535f5cf05c0a9cad4c276d5b11156bd077ceef8
Abhishek Sankar <abhishek.sankar.in@gmail.com>
2025-06-01 00:45:28 -0400

    Blog: Screenshotty

--- src/components/blogs.ts
+++ src/components/blogs.ts
@@ -1,4 +1,5 @@
 import { Blog_CET_Placements } from "../blogs/Blog_CET_Placements";
-import { Blog_Anything_API } from "../blogs/Blog_Anything_API";
+import { Blog_Anything_API } from "../blogs/Blog_Anything_API"; 
+import { Blog_Screenshotty } from "../blogs/Blog_Screenshotty";
 
-export { Blog_CET_Placements, Blog_Anything_API };
+export { Blog_CET_Placements, Blog_Anything_API, Blog_Screenshotty };
